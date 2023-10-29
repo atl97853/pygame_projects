d8fc0d400d8f7c425e62d183c36141f6f119ea69
redpanda97585 <128220069+redpanda97585@users.noreply.github.com>
2023-10-29 02:58:57 -0600

    Define game structure

---
+++ tetris_game/main.py
@@ -0,0 +1,10 @@
+import pygame
+
+# Game Structure 
+
+# Definitions 
+    # Defining the variables needed
+    # Creating the game objects 
+# Game Loop
+    # Updating the positions of the game objects 
+    # Checking for collisions
