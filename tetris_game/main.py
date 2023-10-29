85aa9de301328299a0e9b8249d21dd6e33b0a63a
redpanda97585 <128220069+redpanda97585@users.noreply.github.com>
2023-10-29 03:15:28 -0600

    Set screen display, create game loop and event handler

--- tetris_game/main.py
+++ tetris_game/main.py
@@ -1,10 +1,32 @@
-import pygame
-
 # Game Structure 
+# Definitions --------------------------------------------------
+import pygame, sys
 
-# Definitions 
+pygame.init()
+
     # Defining the variables needed
+DARK_BLUE = (44, 44, 127)
+
+screen = pygame.display.set_mode((300, 600))
+pygame.display.set_caption('Python Tetris')
+
+clock = pygame.time.Clock()
     # Creating the game objects 
-# Game Loop
+
+# Game Loop --------------------------------------------------
+    # 1. Event Handling 
+    # 2. Updating Positions 
+    
+while True:
+    for event in pygame.event.get():
+        print(event.type)
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    
+    # 3. Drawing Objects
+    screen.fill(DARK_BLUE)
+    pygame.display.update()
+    clock.tick(60)
     # Updating the positions of the game objects 
     # Checking for collisions
