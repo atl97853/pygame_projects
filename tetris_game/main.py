07b2144854b0ee1a41f4b825ea60b205cf5e213c
redpanda97585 <128220069+redpanda97585@users.noreply.github.com>
2023-11-26 23:12:07 -0600

    Create game_grid object

--- tetris_game/main.py
+++ tetris_game/main.py
@@ -1,6 +1,7 @@
 # Game Structure 
 # Definitions --------------------------------------------------
 import pygame, sys
+from grid import Grid
 
 pygame.init()
 
@@ -11,6 +12,9 @@ screen = pygame.display.set_mode((300, 600))
 pygame.display.set_caption('Python Tetris')
 
 clock = pygame.time.Clock()
+
+game_grid = Grid()
+game_grid.print_grid()
     # Creating the game objects 
 
 # Game Loop --------------------------------------------------
@@ -19,7 +23,7 @@ clock = pygame.time.Clock()
     
 while True:
     for event in pygame.event.get():
-        print(event.type)
+        # print(event.type) # just for testing 
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
