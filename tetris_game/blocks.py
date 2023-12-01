2fbf7a07e8f241a4d5e98186dd99fbe2801a14bd
redpanda97585 <128220069+redpanda97585@users.noreply.github.com>
2023-11-30 23:00:14 -0600

    Debug

--- tetris_game/blocks.py
+++ tetris_game/blocks.py
@@ -2,7 +2,7 @@ from block import Block
 from position import Position
 
 class LBlock(Block):
-    def __init__(self, id):
+    def __init__(self):
         super().__init__(id = 1)
         self.cells = {
             0:[Position(0, 2),Position(1, 0), Position(1, 1), Position(1, 2)],
